2fae09317a39420c313786d9e0613d650d8eb105
Wesley Ellis <tahnok@gmail.com>
2016-10-10 12:23:15 -0400

    working bluetooth colour setting

---
+++ bluetooth.cpp
@@ -0,0 +1,90 @@
+#include <Arduino.h>
+#include <Adafruit_BLE.h>
+#include <Adafruit_BluefruitLE_SPI.h>
+
+#define BUFSIZE                        128   // Size of the read buffer for incoming data
+#define VERBOSE_MODE                   true  // If set to 'true' enables debug output
+#define BLE_READPACKET_TIMEOUT         500   // Timeout in ms waiting to read a response
+
+#define BLUEFRUIT_SPI_CS               8
+#define BLUEFRUIT_SPI_IRQ              7
+#define BLUEFRUIT_SPI_RST              4    // Optional but recommended, set to -1 if unused
+
+//packet stuff
+#define PACKET_ACC_LEN                  (15)
+#define PACKET_GYRO_LEN                 (15)
+#define PACKET_MAG_LEN                  (15)
+#define PACKET_QUAT_LEN                 (19)
+#define PACKET_BUTTON_LEN               (5)
+#define PACKET_COLOR_LEN                (6)
+#define PACKET_LOCATION_LEN             (15)
+
+#define READ_BUFSIZE                    (20)
+
+
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* Buffer to hold incoming characters */
+uint8_t packetbuffer[READ_BUFSIZE + 1];
+
+
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout) {
+  uint16_t origtimeout = timeout, replyidx = 0;
+
+  memset(packetbuffer, 0, READ_BUFSIZE);
+
+  while (timeout--) {
+    if (replyidx >= 20) break;
+    if ((packetbuffer[1] == 'A') && (replyidx == PACKET_ACC_LEN))
+      break;
+    if ((packetbuffer[1] == 'G') && (replyidx == PACKET_GYRO_LEN))
+      break;
+    if ((packetbuffer[1] == 'M') && (replyidx == PACKET_MAG_LEN))
+      break;
+    if ((packetbuffer[1] == 'Q') && (replyidx == PACKET_QUAT_LEN))
+      break;
+    if ((packetbuffer[1] == 'B') && (replyidx == PACKET_BUTTON_LEN))
+      break;
+    if ((packetbuffer[1] == 'C') && (replyidx == PACKET_COLOR_LEN))
+      break;
+    if ((packetbuffer[1] == 'L') && (replyidx == PACKET_LOCATION_LEN))
+      break;
+
+    while (ble->available()) {
+      char c =  ble->read();
+      if (c == '!') {
+        replyidx = 0;
+      }
+      packetbuffer[replyidx] = c;
+      replyidx++;
+      timeout = origtimeout;
+    }
+
+    if (timeout == 0) break;
+    delay(1);
+  }
+
+  packetbuffer[replyidx] = 0;  // null term
+
+  if (!replyidx)  // no data or timeout
+    return 0;
+  if (packetbuffer[0] != '!')  // doesn't start with '!' packet beginning
+    return 0;
+
+  // check checksum!
+  uint8_t xsum = 0;
+  uint8_t checksum = packetbuffer[replyidx - 1];
+
+  for (uint8_t i = 0; i < replyidx - 1; i++) {
+    xsum += packetbuffer[i];
+  }
+  xsum = ~xsum;
+
+  // Throw an error message if the checksum's don't match
+  if (xsum != checksum) {
+    return 0;
+  }
+
+  // checksum passed!
+  return replyidx;
+}
--- rule110_dress.ino
+++ rule110_dress.ino
@@ -1,4 +1,8 @@
+#include <string.h>
+#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_BLE.h>
+#include <Adafruit_BluefruitLE_SPI.h>
 
 #define PIN 6
 #define ROWS 8
@@ -10,47 +14,104 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PINS, PIN, NEO_GRB + NEO_KHZ800);
 
+
+extern Adafruit_BluefruitLE_SPI ble;
+uint8_t readPacket(Adafruit_BLE *ble, uint16_t timeout);
+extern uint8_t packetbuffer[];
+
 byte start[] = { 1, 0, 1, 1, 1, 0, 0, 1};
 //byte start[WIDTH] = {0};
 
 byte* row1;
 byte* row2;
+uint32_t colour1;
+uint32_t colour2;
+
+
+void error() {
+  pinMode(13, OUTPUT);
+  while (1) {
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);              // wait for a second
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);
+  }
+}
 
 void setup() {
+  if (!ble.begin(false)) {
+    error();
+  }
+
+  if (!ble.factoryReset()) {
+    error();
+  }
+
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
   strip.begin();
   strip.setBrightness(16);
   strip.show();
 
   row1 = start;
   row2 = (byte*) calloc(WIDTH, sizeof(byte));
+
+  colour1 = Wheel(25);
+  colour2 = Wheel(100);
 }
 
 byte index = 0;
 
-void loop() {
-  if (index & 1) {
-    displayRow(row2, index);
-    apply(row2, row1);
-  } else {
-    displayRow(row1, index);
-    apply(row1, row2);
+boolean colourFlop;
 
-  }
-  if (index >= HEIGHT - 1) {
-    index = 0;
-    for(;;);
+void loop() {
+
+  if (ble.available()) {
+    uint8_t len = readPacket(&ble, 1000);
+    if (len == 0) return;
+    if (packetbuffer[1] == 'C') {
+      uint8_t red = packetbuffer[2];
+      uint8_t green = packetbuffer[3];
+      uint8_t blue = packetbuffer[4];
+      if (colourFlop) {
+        colour1 = strip.Color(red, green, blue);
+      } else {
+        colour2 = strip.Color(red, green, blue);
+      }
+      colourFlop = !colourFlop;
+      resetDisplay();
+    }
   } else {
-    index = index + 1;
+    if (index & 1) {
+      displayRow(row2, index);
+      apply(row2, row1);
+    } else {
+      displayRow(row1, index);
+      apply(row1, row2);
+    }
+
+    if (index >= HEIGHT - 1) {
+      resetDisplay();
+    } else {
+      index = index + 1;
+    }
+    delay(100);
   }
-  delay(100);
+}
+
+void resetDisplay() {
+  for (uint16_t i = 0; i < NUM_PINS; i++) {
+    strip.setPixelColor(i, 0);
+  }
+  index = 0;
 }
 
 void displayRow(byte* row, byte column) {
   for (byte i = 0; i < WIDTH; i++) {
     if (row[i]) {
-      setColour(i, column, Wheel(25));
+      setColour(i, column, colour1);
     } else {
-      setColour(i, column, Wheel(100));
+      setColour(i, column, colour2);
     }
   }
   strip.show();
